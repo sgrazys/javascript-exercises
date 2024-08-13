42d50f45bbc680333f4a2386b78ea44efeb8c87d
Saulius Grazys <s.grazys@gmail.com>
2024-08-13 23:46:58 +0300

    add completed exercise nr.5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+	const listOfNums = [];
 
+	if (start > end) {
+		for (let i = end; i <= start; i++) {
+			listOfNums.push(i);
+		}
+	}
+
+	// prettier-ignore
+	if (start <= 0 || 
+    end <= 0 || 
+    start % 1 !== 0 || 
+    end % 1 !== 0 ||
+    typeof(start) !== 'number' ||
+    typeof(end) !== 'number') {
+
+		return 'ERROR';
+	}
+
+	for (let i = start; i <= end; i++) {
+		listOfNums.push(i);
+	}
+
+	return listOfNums.reduce((t, curr) => t + curr, 0);
 };
 
 // Do not edit below this line
