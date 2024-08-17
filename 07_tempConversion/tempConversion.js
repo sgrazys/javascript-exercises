bd084c348a49ede9a6ddf6f4755d2915e6eb44d8
Saulius Grazys <s.grazys@gmail.com>
2024-08-17 12:55:43 +0300

    add completed exercise nr.7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+	const celsius = (fahrenheit - 32) * (5 / 9);
+	const roundedCelsius = Number(celsius.toFixed(1));
+
+	return roundedCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+	const fahrenheit = celsius * (9 / 5) + 32;
+	const roundedFahrenheit = Number(fahrenheit.toFixed(1));
+
+	return roundedFahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
